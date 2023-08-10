f4e926c4b0fc21f7839f7e73cbc0ff04af83eb43
Mark D. Lalich <34788138+lalich@users.noreply.github.com>
2023-08-09 17:08:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,83 @@
 require("dotenv").config()
-const morgan = require("morgan")
+const {PORT, DATABASE_URL} = process.env
+
 const express = require("express")
-const mongoose = require('mongoose')
-const cors = require('cors')
+
+
 const app = express()
-const {PORT,DATABASE_URL} = process.env
-/// Middlesware
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+const morgan = require("morgan")
+
 ///
+
 // MongoDB Conection
-mongoose.connect(DATABASE_URL)
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
 mongoose.connection
 .on('open', () => console.log("i'm connnected"))
 .on('close', () => console.log("i'm Disconected"))
 .on('error', (error) => console.log(error))
 //
-/// book Models
-const bookSchema = new mongoose.Schema({
+const BookSchema = new mongoose.Schema({
     name: String,
     url: String
 })
-const book = mongoose.model('book', bookSchema)
+const Book = mongoose.model('book', BookSchema)
 
-app.get("/book", (req, res) => {
-    res.send("hello world")
-})
+/// Middlesware
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+/// book Models
 
-app.get('/',(req,res)=>{
+// IDUCS
+app.get("/book", async (req, res) => {
+    try {
+        res.json(await Book.find({}))
+      
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+app.post("/book", async (req, res) => {
+    try {
+        res.json(await Book.create(req.body))
+
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+app.get("/book/:id", async (req,res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        res.json(book)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+app.put("/book/:id", async (req, res) => {
+    try {
+        res.json(await Book.findByIdAndUpdate(req.params.id))
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+app.delete("/book/:id", async (req,res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id)
+        res.status(204).json(book)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+// test route
+app.get('/', (req,res) => {
     res.json({Name:"test route "})
 })
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
